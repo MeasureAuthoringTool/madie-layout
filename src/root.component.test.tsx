ca5fc0510dcefee25ea8f62d179509eed25c825d
Gregory P. Akins <gregory.akins@semanticbits.com>
2021-09-13 11:56:04 -0400

    MAT-3190: Adding menu to layout for public site

--- src/root.component.test.tsx
+++ src/root.component.test.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import * as React from "react";
 import { fireEvent, screen, render } from "@testing-library/react";
 import Root from "./root.component";
@@ -5,7 +7,7 @@ import Root from "./root.component";
 describe("Root component", () => {
   it("should be in the document", () => {
     const { getByText } = render(<Root name="Testapp" />);
-    expect(getByText(/Testapp is mounted!/i)).toBeInTheDocument();
+    expect(getByText(/Release Notes/i)).toBeInTheDocument();
   });
 
   it("should mount editor component", async () => {
