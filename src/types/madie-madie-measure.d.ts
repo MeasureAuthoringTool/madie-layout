6c0716dae7888ae856ab80246f537de876ec6d5e
Gregory P. Akins <gregory.akins@semanticbits.com>
2021-11-04 08:07:25 -0400

    MAT-3353: Adding madie-measure type for tests mocks

--- src/types/madie-madie-measure.d.ts
+++ src/types/madie-madie-measure.d.ts
@@ -2,7 +2,7 @@ declare module "@madie/madie-measure" {
   import { FC } from "react";
   import { LifeCycleFn } from "single-spa";
 
-  export const Madiemeasure: FC;
+  export const MadieMeasure: FC;
   export const bootstrap: LifeCycleFn<void>;
   export const mount: LifeCycleFn<void>;
   export const unmount: LifeCycleFn<void>;
