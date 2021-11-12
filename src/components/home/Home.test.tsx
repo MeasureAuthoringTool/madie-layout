4db9158d54a7da27c9bbef38ac6cee1f8cc4823d
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2021-11-12 11:21:16 -0500

    Add simple button to home for navigation to measure page

--- src/components/home/Home.test.tsx
+++ src/components/home/Home.test.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { render } from "@testing-library/react";
 import Home from "./Home";
 
@@ -16,5 +16,10 @@ describe("Home component", () => {
   it("should mount home component", async () => {
     const { getByText } = render(<Home />);
     expect(getByText(/You are successfully logged in/i)).toBeInTheDocument();
+  });
+
+  it("should have a button for measure page", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("Measures")).toBeInTheDocument();
   });
 });
