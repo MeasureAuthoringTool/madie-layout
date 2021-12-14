ac5564c661f2217c1abc40edac0efab1e3393b31
Joe Kotanchik <56264529+jkotanchik-SB@users.noreply.github.com>
2021-12-14 13:27:31 -0500

    Import useHistory hook

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useHistory } from "react-router-dom";
 import { SecureRoute, LoginCallback } from "@okta/okta-react";
 import Home from "../components/home/Home";
 import Login from "../components/login/Login";
