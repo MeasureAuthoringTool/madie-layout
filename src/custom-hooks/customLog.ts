a21a8d8e8654f1da54c5d93018d258cc3d2cfaf5
CeciliaLiu8 <cecilialiu8@gmail.com>
2022-03-22 07:38:32 -0500

    MAT-3804 log user login and logout

---
+++ src/custom-hooks/customLog.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosResponse } from "axios";
+import { ServiceConfig, getServiceConfig } from "./getServiceConfig";
+
+export default async function customLog(
+  input: any,
+  action: string
+): Promise<AxiosResponse<any, any>> {
+  if (
+    input !== null &&
+    input !== undefined &&
+    Object.keys(input).length !== 0
+  ) {
+    const serviceUrl = await getServiceUrl();
+
+    return axios.post(`${serviceUrl}/log/${action}`, input);
+  }
+}
+
+export const getServiceUrl = async () => {
+  const config: ServiceConfig = await getServiceConfig();
+  const serviceUrl: string = config?.loggingService?.baseUrl;
+
+  return serviceUrl;
+};
+
+export const loginLogger = (content) => {
+  return customLog(content, "login");
+};
+
+export const logoutLogger = (content) => {
+  return customLog(content, "logout");
+};
