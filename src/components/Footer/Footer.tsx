86ba6dce025bb3fad2eff4ffaba84e2cb8ac904b
Eric Lawrence <98662008+sm-ericlawrence@users.noreply.github.com>
2022-03-29 14:17:02 -0500

    Tests passing

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { Footer as QPPFooter } from "@madie/madie-design-system/dist/react";
 import "../../styles/FooterStyles.scss";
+import hhsLogoPath from "../../assets/images/hhs-logo-black.svg";
+import qppLogoPath from "../../assets/images/measure_authoring_logo.svg";
 
 const Footer = () => {
+  const assets = {
+    hhsLogo: (
+      <img
+        alt="Department of Health &amp; Human Services USA"
+        className="hhs-logo"
+        data-testid="custom-hhs-logo"
+        src={hhsLogoPath}
+      />
+    ),
+    qppLogo: (
+      <img
+        alt="qpp logo"
+        className="qpp-logo"
+        data-testid="custom-qpp-logo"
+        src={qppLogoPath}
+      />
+    ),
+  };
   return (
     <section className="footer-section">
-      <QPPFooter isNewFooter={true} />
+      <QPPFooter isNewFooter={true} assets={assets} />
     </section>
   );
 };
