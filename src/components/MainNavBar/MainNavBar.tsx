fb7b6f0127a95caa63e56889a601e01c490a1960
mcmcphillips <matt.mcphillips@semanticbits.com>
2022-05-11 12:35:13 -0700

    Remove unused imports

--- src/components/MainNavBar/MainNavBar.tsx
+++ src/components/MainNavBar/MainNavBar.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import logo from "../../assets/images/Logo.svg";
 import { DropDown, DropMenu } from "../../styles/styles";
 import { useOktaAuth } from "@okta/okta-react";
-import { NavLink, useHistory } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import UserProfile from "./UserProfile";
 import UserAvatar from "./UserAvatar";
 import UMLSDialog from "./UMLSDialog";
