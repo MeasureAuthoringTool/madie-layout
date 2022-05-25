a71a247953ba21de668d37824a6090f67fdf3d3d
mcmcphillips <matt.mcphillips@semanticbits.com>
2022-05-25 12:46:06 -0700

    Update casing on measureSchemaValidator

--- src/models/MeasureSchemaValidator.ts
+++ src/models/MeasureSchemaValidator.ts
@@ -4,22 +4,22 @@ import { MeasureScoring } from "@madie/madie-models/dist/MeasureScoring";
 
 export const MeasureSchemaValidator = Yup.object().shape({
   measureName: Yup.string()
-    .max(500, "A measure name cannot be more than 500 characters.")
-    .required("A measure name is required.")
-    .matches(/[a-zA-Z]/, "A Measure name must contain at least one letter.")
+    .max(500, "A Measure Name cannot be more than 500 characters.")
+    .required("A Measure Name is required.")
+    .matches(/[a-zA-Z]/, "A Measure Name must contain at least one letter.")
     .matches(/^((?!_).)*$/, "Measure Name must not contain '_' (underscores)."),
   model: Yup.string()
     .oneOf(Object.values(Model))
-    .required("A Measure model is required."),
+    .required("A Measure Model is required."),
   cqlLibraryName: Yup.string()
-    .required("Measure library name is required.")
+    .required("Measure Lbrary name is required.")
     .matches(
       /^((?!_).)*$/,
-      "Measure library name must not contain '_' (underscores)."
+      "Measure Library name must not contain '_' (underscores)."
     )
     .matches(
       /^[A-Z][a-zA-Z0-9]*$/,
-      "Measure library name must start with an upper case letter, followed by alpha-numeric character(s) and must not contain spaces or other special characters."
+      "Measure Library name must start with an upper case letter, followed by alpha-numeric character(s) and must not contain spaces or other special characters."
     ),
   measureScoring: Yup.string()
     .oneOf(Object.values(MeasureScoring))
