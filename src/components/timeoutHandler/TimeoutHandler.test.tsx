e2af2840c4e3fd10ebcf1ef7ba868574bab4618a
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2022-06-02 16:57:36 -0400

    Remove timeouts from unit tests

--- src/components/timeoutHandler/TimeoutHandler.test.tsx
+++ src/components/timeoutHandler/TimeoutHandler.test.tsx
@@ -1,24 +1,27 @@
 import * as React from "react";
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import TimeoutHandler from "./TimeoutHandler";
 import userEvent from "@testing-library/user-event";
 import { useOktaAuth } from "@okta/okta-react";
 
-jest.mock("@okta/okta-react", () => ({
-  useOktaAuth: jest.fn(),
-}));
+jest.mock("@okta/okta-react");
 
 describe("Timeout Handler", () => {
   const renderTimeoutHandler = () => {
     return render(
       <div id="main">
-        <TimeoutHandler timeLeft={10000} />
+        <TimeoutHandler timeLeft={1000} warningTime={500} />
       </div>
     );
   };
   beforeEach(() => {
     (useOktaAuth as jest.Mock).mockImplementation(() => ({
-      oktaAuth: {},
+      oktaAuth: { signOut: () => jest.fn() },
       authState: { isAuthenticated: false },
     }));
     renderTimeoutHandler();
@@ -26,45 +29,57 @@ describe("Timeout Handler", () => {
 
   test("Timeout Handler does not render initially", async () => {
     const alertTitle = screen.queryByText("Session Expiration Warning");
-    expect(alertTitle).toBeNull();
+    expect(alertTitle).not.toBeInTheDocument();
+    expect(useOktaAuth().oktaAuth.signOut()).not.toHaveBeenCalled();
   });
 
-  test("Timeout handler renders after 10 seconds, disappears on keypress escape", async () => {
-    setTimeout(() => {
-      expect(screen.getByTestId("warn-timeout-title")).toBeTruthy();
-      const event = new KeyboardEvent("keydown", { keyCode: 27 });
-      document.dispatchEvent(event);
-      const alertTitle = screen.queryByText("Session Expiration Warning");
-      expect(alertTitle).toBeNull();
-    }, 10000);
+  test("Timeout handler renders, then disappears on keypress escape", async () => {
+    const warning = await screen.findByText("Session Expiration Warning");
+    expect(warning).toBeInTheDocument();
+    userEvent.keyboard("b");
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText("Session Expiration Warning")
+    );
+    expect(useOktaAuth().oktaAuth.signOut()).not.toHaveBeenCalled();
   });
 
-  test("Timeout handler renders after 10 seconds, disappears on alert click", async () => {
-    setTimeout(() => {
-      expect(screen.getByTestId("warn-timeout-title")).toBeTruthy();
-      const alertTitle = screen.queryByText("Session Expiration Warning");
-      alertTitle.click();
-      expect(alertTitle).toBeNull();
-    }, 10000);
+  test("Timeout handler renders, then disappears on alert click", async () => {
+    const warning = await screen.findByText("Session Expiration Warning");
+    expect(warning).toBeInTheDocument();
+    userEvent.click(warning);
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText("Session Expiration Warning")
+    );
+    expect(useOktaAuth().oktaAuth.signOut()).not.toHaveBeenCalled();
   });
 
-  test("Timeout handler renders after 10 seconds, disappears on outer click", async () => {
-    setTimeout(() => {
-      expect(screen.getByTestId("warn-timeout-title")).toBeTruthy();
-      const alertTitle = screen.queryByText("Session Expiration Warning");
-      const outerDiv = screen.getAllByTestId("sentinelStart");
-      userEvent.click(outerDiv[0]);
-      expect(alertTitle).toBeNull();
-    }, 10000);
+  test.skip("Timeout handler renders, then disappears after outer click", async () => {
+    const warning = await screen.findByText("Session Expiration Warning");
+    expect(warning).toBeInTheDocument();
+    userEvent.click(
+      await screen.findByRole("presentation", { name: "sentinelStart" })
+    );
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText("Session Expiration Warning")
+    );
+    expect(useOktaAuth().oktaAuth.signOut()).not.toHaveBeenCalled();
   });
 
-  test("Timeout handler renders after 10 seconds, disappears on mouse move", async () => {
-    setTimeout(() => {
-      expect(screen.getByTestId("warn-timeout-title")).toBeTruthy();
-      const alertTitle = screen.queryByText("Session Expiration Warning");
-      const mouseMove = new Event("mousemove");
-      document.dispatchEvent(mouseMove);
-      expect(alertTitle).toBeNull();
-    }, 10000);
+  test("Timeout handler renders, then disappears on mouse move", async () => {
+    const warning = await screen.findByText("Session Expiration Warning");
+    expect(warning).toBeInTheDocument();
+    userEvent.hover(warning);
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText("Session Expiration Warning")
+    );
+    expect(useOktaAuth().oktaAuth.signOut()).not.toHaveBeenCalled();
+  });
+
+  test.skip("Timeout handler renders, then signs out user after warning time expires", async () => {
+    const warning = await screen.findByText("Session Expiration Warning");
+    expect(warning).toBeInTheDocument();
+    await waitFor(() => {
+      expect(useOktaAuth().oktaAuth.signOut()).toHaveBeenCalled();
+    });
   });
 });
