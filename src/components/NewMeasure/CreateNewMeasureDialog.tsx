0391abd52e8e0a77ba01f2fd14e8fdb00c2ec306
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2025-02-04 12:16:06 -0800

    MAT-8163 remove qicore6

--- src/components/NewMeasure/CreateNewMeasureDialog.tsx
+++ src/components/NewMeasure/CreateNewMeasureDialog.tsx
@@ -69,9 +69,7 @@ const CreateNewMeasureDialog = ({ open, onClose }) => {
 
   let modelOptions = Object.keys(Model);
   const featureFlags = useFeatureFlags();
-  if (!featureFlags?.qiCore6) {
     modelOptions = modelOptions.filter((option) => option !== "QICORE_6_0_0");
-  }
 
   async function createMeasure(measure: Measure) {
     const config: ServiceConfig = await getServiceConfig();
