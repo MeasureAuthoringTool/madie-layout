c26948d02cb28e9ccd68701bbe7c9f5bfacdef8a
mcmcphillips <matt.mcphillips@icf.com>
2025-08-14 14:17:45 -0700

    MAT-8510: Fix more 508 issues. Fix navigation on keyboard, pull anchor outside of render cycle.

--- src/components/PageHeader/exportAction/ExportAction.tsx
+++ src/components/PageHeader/exportAction/ExportAction.tsx
@@ -1,6 +1,13 @@
-import React, { useState } from "react";
-import { IconButton } from "@mui/material";
-import { Popover } from "@madie/madie-design-system/dist/react";
+import React, { useRef, useState } from "react";
+import {
+  ClickAwayListener,
+  Grow,
+  IconButton,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+} from "@mui/material";
 import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
 
 interface PropTypes {
@@ -10,39 +17,89 @@ interface PropTypes {
 export default function ExportAction(props: PropTypes) {
   const { onClick } = props;
 
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [open, setOpen] = useState(false);
+  // move anchorElement to a stable reference that does not change across renders.
+  const anchorRef = useRef<HTMLButtonElement>(null);
 
   const handleOptionClick = (option: "Export" | "Export for Publishing") => {
     onClick(option);
-    setAnchorEl(null);
+    setOpen(false);
   };
+
+  function handleListKeyDown(event: React.KeyboardEvent) {
+    // it's inside of another key trap. If we allow the event to bubble up, the action center speed dial will take the command instead of the child element here.
+    event.stopPropagation();
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  }
 
   return (
     <>
       <IconButton
-        onClick={(event) => setAnchorEl(event.currentTarget)}
+        onClick={() => {
+          setOpen(true);
+        }}
         data-testid="export-action-btn"
+        ref={anchorRef}
+        style={{ height: "40px" }}
       >
-        <FileUploadOutlinedIcon />
+        <span>
+          <FileUploadOutlinedIcon />
+        </span>
       </IconButton>
-      <Popover
-        optionsOpen={Boolean(anchorEl)}
-        anchorEl={anchorEl}
-        handleClose={() => setAnchorEl(null)}
-        additionalSelectOptionProps={[
-          {
-            label: "Export",
-            dataTestId: "export-option",
-            toImplementFunction: () => handleOptionClick("Export"),
-          },
-          {
-            label: "Export for Publishing",
-            dataTestId: "export-publishing-option",
-            toImplementFunction: () =>
-              handleOptionClick("Export for Publishing"),
-          },
-        ]}
-      />
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        placement="bottom-start"
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom-start" ? "left top" : "left bottom",
+            }}
+          >
+            <Paper>
+              <ClickAwayListener
+                onClickAway={() => {
+                  setOpen(false);
+                }}
+              >
+                <MenuList
+                  autoFocusItem={open}
+                  id="share-menu"
+                  onKeyDown={handleListKeyDown}
+                >
+                  <MenuItem
+                    data-testId="export-option"
+                    onClick={(e) => {
+                      handleOptionClick("Export");
+                    }}
+                  >
+                    Export
+                  </MenuItem>
+                  <MenuItem
+                    data-testId="export-publishing-option"
+                    onClick={(e) => {
+                      handleOptionClick("Export for Publishing");
+                    }}
+                  >
+                    Export for Publishing
+                  </MenuItem>
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
     </>
   );
 }
