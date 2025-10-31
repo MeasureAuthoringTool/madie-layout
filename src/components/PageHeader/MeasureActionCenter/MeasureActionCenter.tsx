8b9472bdcb8490551355912eedff782ff6b94915
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2025-10-31 09:13:23 -0700

    MAT-9271 variable change

--- src/components/PageHeader/MeasureActionCenter/MeasureActionCenter.tsx
+++ src/components/PageHeader/MeasureActionCenter/MeasureActionCenter.tsx
@@ -206,7 +206,7 @@ const MeasureActionCenter = (props: PropTypes) => {
       });
     }
 
-    if (featureFlags?.TransferMeasure && isOwnerOfMeasure(measure)) {
+    if (featureFlags?.TransferMeasure && ownerOfMeasure) {
       actions.set("transfer measure", {
         icon: (
           <TransferAction
